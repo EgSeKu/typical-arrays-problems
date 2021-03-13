dbd52c90d8395ad009fe0d09f32021b369f8d425
Egor Kuzmichev <kuzmichev_2497@mail.ru>
2021-03-13 13:12:51 +0300

    docs: edit index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+exports.min = function min(arr) {
+    let res = 0;
+    if (typeof arr == 'undefined'||arr.length == 0)
+        return 0;
+    for (i = 0; i < arr.length; i++)
+        if (arr[i] < res)
+            res = arr[i];
+    return res;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(arr) {
+    let res = 0;
+    if (typeof arr =='undefined'||arr.length == 0)
+        return 0;
+    for (i = 0; i < arr.length; i++)
+        if (arr[i] > res)
+            res = arr[i];
+    return res;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(arr) {
+    let res = 0;
+    if (typeof arr =='undefined'||arr.length == 0)
+        return 0;
+    for (i = 0; i < arr.length; i++)
+            res += arr[i];
+    return res/(arr.length);
 }
